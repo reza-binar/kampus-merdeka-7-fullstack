1363cb3b695eba937b0ea8e3b9843191e8dbf323
Fahmi Alfareza <falfareza@binaracademy.org>
2024-09-24 20:14:33 +0700

    chapter 2: topic 4

--- chapter-2/3-dom/scripts/index.js
+++ chapter-2/3-dom/scripts/index.js
@@ -1,62 +1,32 @@
-// Import json data from data/students.json with module
-import studentsData from "../data/students.json" with { type: "json" };
-// with commonjs
-// const studentsData = require("../data/students.json")
-
-/* Start to show all students */
-// Get id of student-content to control the view
 const studentContent = document.getElementById("student-content");
-
-// Helper variable
-let studentContentHTML = "";
-studentsData.map((student) => {
-    // variable that will be show in student-content id
-    const studentContent = `
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem">
-                <div class="card-body">
-                    <h5 class="card-title">${student.name}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">
-                        ${student.education.bachelor}
-                    </h6>
-                    <p class="card-text">
-                        My name is ${student.name}, used to called ${student.nickName}. I am from ${student.address.city}, ${student.address.province}. And I am student of ${student.education.bachelor}.
-                    </p>
-                </div>
-            </div>
-        </div>
-    `;
-    studentContentHTML += studentContent;
-});
-
-studentContent.innerHTML = "<h1>Loading...</h1>";
-
-// Edit the html content in student-content
-setTimeout(() => {
-    // It will execute after 3 seconds
-    studentContent.innerHTML = studentContentHTML;
-}, 3000);
-/* End to show all students */
-
-/* Start to search the students */
 const search = document.getElementById("search");
 const searchForm = document.getElementById("search-form");
+
+search.addEventListener("input", (e) => {
+    // If the search is change, the function will be running
+    const searchValue = e.target.value.toLowerCase();
+    searchStudentContent(searchValue);
+});
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
 });
 
-search.addEventListener("input", (e) => {
+// Normal function
+async function searchStudentContent(search) {
+    const studentsData = await getStudentData();
+
     studentContent.innerHTML = "<h1>Loading...</h1>";
 
-    // If the search is change, the function will be running
-    const searchValue = e.target.value.toLowerCase();
-
-    // search student by input
+    // search student by input (Backend)
     const filteredStudent = studentsData.filter((student) => {
-        return student.name.toLowerCase().includes(searchValue) || student.education.bachelor.toLowerCase().includes(searchValue);
+        return (
+            student.name.toLowerCase().includes(search) ||
+            student.education.bachelor.toLowerCase().includes(search)
+        );
     });
 
+    // Frontend
     let studentContentHTML = "";
     filteredStudent.map((student) => {
         // variable that will be show in student-content id
@@ -77,7 +47,16 @@ search.addEventListener("input", (e) => {
         `;
         studentContentHTML += studentContent;
     });
-
     studentContent.innerHTML = studentContentHTML;
-});
-/* End to search the students */
+}
+
+// Arrow function, this function is to get students.json data that can be rendered to html file
+const getStudentData = async () => {
+    const response = await fetch("./data/students.json");
+    const data = await response.json();
+
+    return data;
+};
+
+/* Show all student data */
+searchStudentContent("");
