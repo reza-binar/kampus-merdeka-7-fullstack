3069ccaec2c9a7e772fad85a583be27ce32c1a14
Fahmi Alfareza <falfareza@binaracademy.org>
2024-10-08 21:47:56 +0700

    chapter 3: topic 6

--- chapter-3/express-js/src/utils/image-kit.js
+++ chapter-3/express-js/src/utils/image-kit.js
@@ -6,8 +6,6 @@ const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-console.log(process.env);
-
 // Image upload function to imagekit
 exports.imageUpload = async (file) => {
     const uploadedFile = await imagekit.upload({
