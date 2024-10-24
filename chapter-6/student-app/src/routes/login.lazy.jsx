778d4b57a1cfb6d700b19ea939968bb20dcba4ea
Fahmi Alfareza <falfareza@binaracademy.org>
2024-10-24 21:37:36 +0700

    chapter 5: topic 3

---
+++ chapter-6/student-app/src/routes/login.lazy.jsx
@@ -0,0 +1,63 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+export const Route = createLazyFileRoute("/login")({
+    component: Login,
+});
+
+function Login() {
+    return (
+        <Row className="mt-5">
+            <Col className="offset-md-3">
+                <Card className="text-center">
+                    <Card.Header>Login</Card.Header>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="formPlaintextEmail"
+                            >
+                                <Form.Label column sm="2">
+                                    Email
+                                </Form.Label>
+                                <Col sm="10">
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Email"
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="formPlaintextPassword"
+                            >
+                                <Form.Label column sm="2">
+                                    Password
+                                </Form.Label>
+                                <Col sm="10">
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                        <div className="d-grid gap-2">
+                            <Button variant="primary">Login</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col md={3}></Col>
+        </Row>
+    );
+}
