60f4fd5516c469bc54a72c4cb56d421219671142
Fahmi Alfareza <falfareza@binaracademy.org>
2024-11-01 21:41:11 +0700

    chapter 6: topic 2

--- chapter-6/student-app/src/routes/index.lazy.jsx
+++ chapter-6/student-app/src/routes/index.lazy.jsx
@@ -1,13 +1,60 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { getStudents } from "../service/student";
 
 export const Route = createLazyFileRoute("/")({
     component: Index,
 });
 
 function Index() {
+    const { token } = useSelector((state) => state.auth);
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        const getStudentData = async () => {
+            const result = await getStudents();
+            if (result.success) {
+                setStudents(result.data);
+            }
+        };
+
+        if (token) {
+            getStudentData();
+        }
+    }, [token]);
+
     return (
-        <div className="p-2">
-            <h3>Welcome To Wakanda App!</h3>
-        </div>
+        <Row className="mt-4">
+            {!token && (
+                <Col>
+                    <h1>Please login first to get student data!</h1>
+                </Col>
+            )}
+
+            {students.length > 0 &&
+                students.map((student) => (
+                    <Col key={student.id} md={3}>
+                        <Card style={{ width: "18rem" }}>
+                            <Card.Img
+                                variant="top"
+                                src={student.profile_picture}
+                            />
+                            <Card.Body>
+                                <Card.Title>{student?.name}</Card.Title>
+                                <Card.Text>{student?.nick_name}</Card.Text>
+                                <Button variant="primary">
+                                    Detail Student
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+        </Row>
     );
 }
