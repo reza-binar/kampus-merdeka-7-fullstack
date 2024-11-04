6b6fffa43decd59155e4fa777d022a3a95896bed
Fahmi Alfareza <signer_aero0l@icloud.com>
2024-11-04 21:55:17 +0700

    chapter 6: topic 3

--- chapter-6/student-app/src/routeTree.gen.ts
+++ chapter-6/student-app/src/routeTree.gen.ts
@@ -20,6 +20,7 @@ const RegisterLazyImport = createFileRoute('/register')()
 const ProfileLazyImport = createFileRoute('/profile')()
 const LoginLazyImport = createFileRoute('/login')()
 const IndexLazyImport = createFileRoute('/')()
+const StudentsCreateLazyImport = createFileRoute('/students/create')()
 const StudentsIdLazyImport = createFileRoute('/students/$id')()
 
 // Create/Update Routes
@@ -47,6 +48,14 @@ const IndexLazyRoute = IndexLazyImport.update({
   path: '/',
   getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
+
+const StudentsCreateLazyRoute = StudentsCreateLazyImport.update({
+  id: '/students/create',
+  path: '/students/create',
+  getParentRoute: () => rootRoute,
+} as any).lazy(() =>
+  import('./routes/students/create.lazy').then((d) => d.Route),
+)
 
 const StudentsIdLazyRoute = StudentsIdLazyImport.update({
   id: '/students/$id',
@@ -93,6 +102,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof StudentsIdLazyImport
       parentRoute: typeof rootRoute
     }
+    '/students/create': {
+      id: '/students/create'
+      path: '/students/create'
+      fullPath: '/students/create'
+      preLoaderRoute: typeof StudentsCreateLazyImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -104,6 +120,7 @@ export interface FileRoutesByFullPath {
   '/profile': typeof ProfileLazyRoute
   '/register': typeof RegisterLazyRoute
   '/students/$id': typeof StudentsIdLazyRoute
+  '/students/create': typeof StudentsCreateLazyRoute
 }
 
 export interface FileRoutesByTo {
@@ -112,6 +129,7 @@ export interface FileRoutesByTo {
   '/profile': typeof ProfileLazyRoute
   '/register': typeof RegisterLazyRoute
   '/students/$id': typeof StudentsIdLazyRoute
+  '/students/create': typeof StudentsCreateLazyRoute
 }
 
 export interface FileRoutesById {
@@ -121,14 +139,34 @@ export interface FileRoutesById {
   '/profile': typeof ProfileLazyRoute
   '/register': typeof RegisterLazyRoute
   '/students/$id': typeof StudentsIdLazyRoute
+  '/students/create': typeof StudentsCreateLazyRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/login' | '/profile' | '/register' | '/students/$id'
+  fullPaths:
+    | '/'
+    | '/login'
+    | '/profile'
+    | '/register'
+    | '/students/$id'
+    | '/students/create'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/login' | '/profile' | '/register' | '/students/$id'
-  id: '__root__' | '/' | '/login' | '/profile' | '/register' | '/students/$id'
+  to:
+    | '/'
+    | '/login'
+    | '/profile'
+    | '/register'
+    | '/students/$id'
+    | '/students/create'
+  id:
+    | '__root__'
+    | '/'
+    | '/login'
+    | '/profile'
+    | '/register'
+    | '/students/$id'
+    | '/students/create'
   fileRoutesById: FileRoutesById
 }
 
@@ -138,6 +176,7 @@ export interface RootRouteChildren {
   ProfileLazyRoute: typeof ProfileLazyRoute
   RegisterLazyRoute: typeof RegisterLazyRoute
   StudentsIdLazyRoute: typeof StudentsIdLazyRoute
+  StudentsCreateLazyRoute: typeof StudentsCreateLazyRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
@@ -146,6 +185,7 @@ const rootRouteChildren: RootRouteChildren = {
   ProfileLazyRoute: ProfileLazyRoute,
   RegisterLazyRoute: RegisterLazyRoute,
   StudentsIdLazyRoute: StudentsIdLazyRoute,
+  StudentsCreateLazyRoute: StudentsCreateLazyRoute,
 }
 
 export const routeTree = rootRoute
@@ -164,7 +204,8 @@ export const routeTree = rootRoute
         "/login",
         "/profile",
         "/register",
-        "/students/$id"
+        "/students/$id",
+        "/students/create"
       ]
     },
     "/": {
@@ -181,6 +222,9 @@ export const routeTree = rootRoute
     },
     "/students/$id": {
       "filePath": "students/$id.lazy.jsx"
+    },
+    "/students/create": {
+      "filePath": "students/create.lazy.jsx"
     }
   }
 }
---
+++ chapter-6/student-app/src/routes/students/create.lazy.jsx
@@ -0,0 +1,173 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { getUniversities } from "../../service/university";
+import { getClasses } from "../../service/class";
+
+export const Route = createLazyFileRoute("/students/create")({
+    component: CreateStudent,
+});
+
+function CreateStudent() {
+    const [name, setName] = useState("");
+    const [nickName, setNickName] = useState("");
+    const [profilePicture, setProfilePicture] = useState(undefined);
+    const [universities, setUniversities] = useState([]);
+    const [universityId, setUniversityId] = useState(0);
+    const [classes, setClasses] = useState([]);
+    const [classId, setClassId] = useState(0);
+
+    useEffect(() => {
+        const getUniversitiesData = async () => {
+            const result = await getUniversities();
+            if (result?.success) {
+                setUniversities(result?.data);
+            }
+        };
+        const getClassesData = async () => {
+            const result = await getClasses();
+            if (result?.success) {
+                setClasses(result?.data);
+            }
+        };
+
+        getUniversitiesData();
+        getClassesData();
+    }, []);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Row className="mt-5">
+            <Col className="offset-md-3">
+                <Card>
+                    <Card.Header className="text-center">
+                        Create Student
+                    </Card.Header>
+                    <Card.Body>
+                        <Form onSubmit={onSubmit}>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="name"
+                            >
+                                <Form.Label column sm={3}>
+                                    Name
+                                </Form.Label>
+                                <Col sm="9">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Name"
+                                        required
+                                        value={name}
+                                        onChange={(event) => {
+                                            setName(event.target.value);
+                                        }}
+                                    />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="nick_name"
+                            >
+                                <Form.Label column sm={3}>
+                                    Nick Name
+                                </Form.Label>
+                                <Col sm="9">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Nick Name"
+                                        required
+                                        value={nickName}
+                                        onChange={(event) => {
+                                            setNickName(event.target.value);
+                                        }}
+                                    />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="nick_name"
+                            >
+                                <Form.Label column sm={3}>
+                                    University
+                                </Form.Label>
+                                <Col sm="9">
+                                    <Form.Select aria-label="Default select example">
+                                        {universities.length > 0 &&
+                                            universities.map((university) => (
+                                                <option
+                                                    key={university?.id}
+                                                    value={university?.id}
+                                                >
+                                                    {university?.name}
+                                                </option>
+                                            ))}
+                                    </Form.Select>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="nick_name"
+                            >
+                                <Form.Label column sm={3}>
+                                    Class
+                                </Form.Label>
+                                <Col sm="9">
+                                    <Form.Select aria-label="Default select example">
+                                        {classes.length > 0 &&
+                                            classes.map((c) => (
+                                                <option
+                                                    key={c?.id}
+                                                    value={c?.id}
+                                                >
+                                                    {c?.class}
+                                                </option>
+                                            ))}
+                                    </Form.Select>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group
+                                as={Row}
+                                className="mb-3"
+                                controlId="profilePicture"
+                            >
+                                <Form.Label column sm={3}>
+                                    Profile Picture
+                                </Form.Label>
+                                <Col sm={9}>
+                                    <Form.Control
+                                        type="file"
+                                        placeholder="Choose File"
+                                        required
+                                        onChange={(event) => {
+                                            setProfilePicture(
+                                                event.target.files[0]
+                                            );
+                                        }}
+                                        accept=".jpg,.png"
+                                    />
+                                </Col>
+                            </Form.Group>
+                            <div className="d-grid gap-2">
+                                <Button type="submit" variant="primary">
+                                    Create Student
+                                </Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col md={3}></Col>
+        </Row>
+    );
+}
