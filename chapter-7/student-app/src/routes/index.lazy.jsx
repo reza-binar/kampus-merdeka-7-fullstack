07b9daf3050cfbe28896ee717f356ff89ebedd52
Fahmi Alfareza <signer_aero0l@icloud.com>
2024-11-11 20:29:47 +0700

    chapter 7: topic 1

---
+++ chapter-7/student-app/src/routes/index.lazy.jsx
@@ -0,0 +1,65 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { getStudents } from "../service/student";
+import StudentItem from "../components/Student/StudentItem";
+
+export const Route = createLazyFileRoute("/")({
+    component: Index,
+});
+
+function Index() {
+    const { token } = useSelector((state) => state.auth);
+
+    const [students, setStudents] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const getStudentData = async () => {
+            setIsLoading(true);
+            const result = await getStudents();
+            if (result.success) {
+                setStudents(result.data);
+            }
+            setIsLoading(false);
+        };
+
+        if (token) {
+            getStudentData();
+        }
+    }, [token]);
+
+    if (!token) {
+        return (
+            <Row className="mt-4">
+                <Col>
+                    <h1 className="text-center">
+                        Please login first to get student data!
+                    </h1>
+                </Col>
+            </Row>
+        );
+    }
+
+    if (isLoading) {
+        return (
+            <Row className="mt-4">
+                <h1>Loading...</h1>
+            </Row>
+        );
+    }
+
+    return (
+        <Row className="mt-4">
+            {students.length === 0 ? (
+                <h1>Student data is not found!</h1>
+            ) : (
+                students.map((student) => (
+                    <StudentItem student={student} key={student?.id} />
+                ))
+            )}
+        </Row>
+    );
+}
